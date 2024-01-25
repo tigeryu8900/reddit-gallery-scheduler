78545c5c195b2135d180d76481c74fa79fe6f17b
tigeryu8900 <tigeryu8900@gmail.com>
2024-01-25 10:42:24 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# reddit-gallery-scheduler
+
+A Reddit scheduler for gallery posts
+
+## Set up
+
+Create a `.env` file in the project root directory in this format where `username` and `password` are the Reddit
+credentials.
+
+```dotenv
+USERNAME=username
+PASSWORD=password
+```
+
+Then, create the `schedule/pending` directory in the project root directory.
+
+Now, for each scheduled post, create a folder of the format `YYYY-mm-dd HH-MM-SS` in `schedule/pending` where the folder
+name corresponds to the scheduled time.
+
+Create a JSON file at `schedule/pending/YYYY-mm-dd HH-MM-SS/data.json` in this format:
+
+```json
+{
+  "subreddit": "r/subreddit",
+  "title": "title",
+  "images": [
+    {
+      "file": "1.png",
+      "caption": "caption",
+      "link": "https://example.com"
+    },
+    {
+      "file": "2.png",
+      "caption": null,
+      "link": null
+    }
+  ],
+  "oc": false,
+  "spoiler": false,
+  "nsfw": false,
+  "flair": "flair",
+  "comments": [
+    "comment 1",
+    "comment 2"
+  ]
+}
+```
+
+Put all the image files in `schedule/pending/YYYY-mm-dd HH-MM-SS/`, and in this case, it would be `1.png` and `2.png`.
+
+Done events are put in `done/`.
---
+++ app.js
@@ -0,0 +1,89 @@
+import puppeteer from "puppeteer";
+import "dotenv/config";
+
+import fs from "fs/promises";
+import * as path from "path";
+
+async function post(dir) {
+    const data = JSON.parse((await fs.readFile(path.join("schedule", "pending", dir, "data.json"))).toString());
+    try {
+        const browser = await puppeteer.launch({headless: "new"});
+        const page = await browser.newPage();
+        await page.goto("https://www.reddit.com/login/");
+        await page.type('#loginUsername', process.env.USERNAME);
+        await page.type('#loginPassword', process.env.PASSWORD);
+        await page.click('.AnimatedForm [type="submit"]');
+        await page.waitForNetworkIdle();
+        await page.goto(`https://www.reddit.com/${data.subreddit}/submit`);
+        await page.waitForSelector('::-p-xpath(//*[text()="Images" or text()="Images & Video"])');
+        await page.click('::-p-xpath(//*[text()="Images" or text()="Images & Video"])');
+        await page.type('[placeholder="Title"]', data.title);
+        for (let image of data.images) {
+            let elementHandle = await page.$('input[type="file"]');
+            await elementHandle.uploadFile(path.join("schedule", dir, image.file));
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+        let divs = [];
+        let time = Date.now();
+        while (divs.length < data.images.length) {
+            if (Date.now() - time > 10000) {
+                throw "Not enough images";
+            }
+            divs = await page.$$('div[draggable="true"]:has([style*="background-image"])');
+        }
+        await divs[0].click();
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        for (let i = data.images.length - 1; i >= 0; --i) {
+            await divs[i].click();
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            if (data.images[i].caption) {
+                await page.type('[placeholder="Add a caption..."]', data.images[i].caption);
+            }
+            if (data.images[i].link) {
+                await page.type('[placeholder="Add a link..."]', data.images[i].link);
+            }
+        }
+        if (data.oc) {
+            await page.click('::-p-xpath(//*[text()="OC"])');
+        }
+        if (data.spoiler) {
+            await page.click('::-p-xpath(//*[text()="Spoiler"])');
+        }
+        if (data.nsfw) {
+            await page.click('::-p-xpath(//*[text()="NSFW"])');
+        }
+        if (data.flair) {
+            await page.click('::-p-xpath(//*[text()="Flair"])');
+            await page.click(`[aria-label="flair_picker"] ::-p-xpath(//*[text()=${JSON.stringify(data.flair)}])`);
+            await page.click('::-p-xpath(//*[text()="Apply"])');
+        }
+        await page.click('::-p-xpath(//*[not(.//i) and text()="Post"])');
+        await page.waitForNavigation();
+        if (data.comments) {
+            let markdown = await page.$('::-p-xpath(//*[text()="Markdown Mode"])');
+            if (markdown) {
+                await markdown.click();
+            }
+            for (let comment of data.comments) {
+                await page.waitForSelector('[placeholder="What are your thoughts?"]');
+                await page.type('[placeholder="What are your thoughts?"]', comment);
+                await page.click('[type="submit"]');
+                await page.waitForNetworkIdle();
+            }
+        }
+        await fs.rename(path.join("schedule", "pending", dir), path.join("schedule", "done", dir));
+        console.log("Posted", dir, data);
+    } catch (e) {
+        console.error("Error", dir, data, e);
+    }
+}
+
+(async () => {
+    await fs.mkdir(path.join("schedule", "pending"), {recursive: true}).catch(() => {});
+    await fs.mkdir(path.join("schedule", "done"), {recursive: true}).catch(() => {});
+    for (let dir of await fs.readdir(path.join("schedule", "pending"))) {
+        let time = Date.parse(dir.replace(/^(\d+)-(\d+)-(\d+) (\d+)-(\d+)-(\d+)$/,
+            "$1-$2-$3T$4:$5:$6"));
+        setTimeout(post, time - Date.now(), dir);
+    }
+})();
